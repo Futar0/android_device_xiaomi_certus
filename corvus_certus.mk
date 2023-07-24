b0bc47c8d5f55d0fd2690380a645cc6a65652357
Futar0 <104120854+Futar0@users.noreply.github.com>
2023-07-25 06:24:09 +0800

    Update corvus_certus.mk

--- corvus_certus.mk
+++ corvus_certus.mk
@@ -18,7 +18,7 @@
 $(call inherit-product, device/xiaomi/certus/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/corvus/config/common_full_phone.mk)
+$(call inherit-product, vendor/corvus/config/common.mk)
 
 
 # Boot animaton
